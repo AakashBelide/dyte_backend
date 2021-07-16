b76b5a81983a75d4bb8ad4b6e88ae1108ca8af98
AakashBelide <aakash.belide@gmail.com>
2021-07-17 03:02:42 +0530

    Molecular webhook

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # dyte_backend
 Webhooks microservices using molecular and express built for Dyte Placement task.
+
+# Instructions
+Routes and request types:
+- http://localhost:3000/api/url/register - POST Request (Paramater: targetURL)
+- http://localhost:3000/api/url/list - GET Request (Paramaters: - )
+- http://localhost:3000/api/url/update - POST Request (Paramaters: id, targetURL)
+- http://localhost:3000/api/url/delete - POST Request (Paramaters: id)
+- http://localhost:3000/api/url/ip - GET Request (Paramaters: IP Address taken from users get request automcatically)
+
+Kindly use [POSTMAN](https://www.postman.com/) or [ThunderClient](https://www.thunderclient.io/) for testing the first four operations. You can use the same for the IP route as well but it might not find the IP address in the headers (x-forwarded-for). You can use ngrok and then use the external address.
+
+
+# microservices [![Moleculer](https://badgen.net/badge/Powered%20by/Moleculer/0e83cd)](https://moleculer.services)
+This is a [Moleculer](https://moleculer.services/)-based microservices project. Generated with the [Moleculer CLI](https://moleculer.services/docs/0.14/moleculer-cli.html).
+
+## Usage
+Start the project with `npm run dev` command. 
+After starting, open the http://localhost:3000/ URL in your browser. 
+On the welcome page you can test the generated services via API Gateway and check the nodes & services.
+
+In the terminal, try the following commands:
+- `nodes` - List all connected nodes.
+- `actions` - List all registered service actions.
+- `call greeter.hello` - Call the `greeter.hello` action.
+- `call greeter.welcome --name John` - Call the `greeter.welcome` action with the `name` parameter.
+
+
+
+## Services
+- **api**: API Gateway services
+- **greeter**: Sample service with `hello` and `welcome` actions.
+
+
+## Useful links
+
+* Moleculer website: https://moleculer.services/
+* Moleculer Documentation: https://moleculer.services/docs/0.14/
+
+## NPM scripts
+
+- `npm run dev`: Start development mode (load all services locally with hot-reload & REPL)
+- `npm run start`: Start production mode (set `SERVICES` env variable to load certain services)
+- `npm run cli`: Start a CLI and connect to production. Don't forget to set production namespace with `--ns` argument in script
+- `npm run ci`: Run continuous test mode with watching
+- `npm test`: Run tests & generate coverage report
+- `npm run dc:up`: Start the stack with Docker Compose
+- `npm run dc:down`: Stop the stack with Docker Compose
---
+++ services/url.service.js
@@ -0,0 +1,200 @@
+"use strict";
+
+// all required modules
+const DBService = require('moleculer-db');
+const { ServiceBroker } = require("moleculer");
+const MongoDBAdapter = require("moleculer-db-adapter-mongo");
+const broker = new ServiceBroker();
+const request = require('request');
+
+// Create a Mongoose service for `post` entities
+
+// const connection_url = "mongodb://localhost/dyte_backend"
+// admin:aroiyuHQpRReBHdE
+const connection_url = "mongodb+srv://admin:aroiyuHQpRReBHdE@cluster0.eswx0.mongodb.net/dyte_backend?retryWrites=true&w=majority"
+broker.createService({
+    name: "urls",
+    mixins: [DBService],
+    adapter: new MongoDBAdapter(connection_url),
+	settings: {
+        fields: ["_id", "targetURL"],
+        entityValidator: {
+			targetURL: "string"
+		}
+    },
+    collection: "urls",
+});
+
+
+broker.start()
+
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+module.exports = {
+	name: "url",
+	settings: {},
+	dependencies: [],
+	actions: {
+		//register
+		register: {
+            params: {
+                targetURL: 'string'
+            },
+			rest: {
+				method: "POST",
+				path: "/register"
+			},
+			async handler(ctx) {
+				const { targetURL } = ctx.params;
+
+                const add = await broker.call("urls.create", {
+					targetURL: targetURL
+				});
+
+				return ("Your unique ID is: " + add._id);
+			}
+		},
+
+		// get whole list of target urls with ids        
+		list: {
+			rest: {
+				method: "GET",
+				path: "/list"
+			},
+			async handler() {
+                const url_list = await broker.call("urls.find");
+				return url_list;
+			}
+		},
+
+		// update targetUrl
+		update: {
+            params: {
+				id: 'string',
+                targetURL: 'string'
+            },
+			rest: {
+				method: "POST",
+				path: "/update"
+			},
+			async handler(ctx) {
+				const { id, targetURL } = ctx.params;
+
+                const add = await broker.call("urls.update", {
+					_id: id,
+					targetURL: targetURL
+				});
+
+				return ("Sucessfully updated the URL.");
+			}
+		},
+
+		// delete a particular id        
+		delete: {
+			params: {
+				id: 'string'
+            },
+			rest: {
+				method: "POST",
+				path: "/delete"
+			},
+			async handler(ctx) {
+				const { id } = ctx.params
+
+                const url_list = await broker.call("urls.remove", { id: id });
+
+				return ("Deleted sucessfully.");
+			}
+		},
+
+		// trigger for ip       
+		ip: {
+			rest: {
+				method: "GET",
+				path: "/ip"
+			},
+			async handler(ctx) {
+				var ip = ctx.options.parentCtx.params.req.headers['x-forwarded-for'];
+
+				if(ip == undefined){
+					ip = "192.168.1.1 (IP address could not be found in the header. Please use ngrok or deploy.)";
+				}
+
+                const url_list = await broker.call("urls.find");
+
+				const no_urls = url_list.length;
+
+				const batch_size = 10;
+
+				const batch = Math.ceil(no_urls/batch_size);
+
+				const rem = no_urls%batch_size;
+
+				//console.log("no_urls: ", no_urls, "; batch_size: ", batch_size);
+				//console.log("batch: ", batch, "; rem: ", rem);
+				
+				for(var i=0; i<batch; i++){
+					if(i == batch-1){
+						var inner_size = rem;
+					}else{
+						var inner_size = batch_size;
+					}
+
+					const unix_timestamp = (new Date()).getTime();
+
+					for(var j=0; j<inner_size; j++){
+						var ind = i*batch_size + j;
+						//console.log("i: ", i, "; j: ", j, "; ind: ", ind);
+						request({
+							url: url_list[ind].targetURL,
+							method: "POST",
+							json: true,
+							body: { ip_address: ip, unix_timestamp: unix_timestamp }
+						}, function (error, response, body) {
+							// code to retry if response is not received
+							if(!response){
+								var retry = 0;
+								var status_code = false;
+								const max_retries = 5;
+								while (retry<max_retries && status_code==false){
+									//console.log("Retrying attempt: ", retry+1);
+									request({
+										url: url_list[ind].targetURL,
+										method: "POST",
+										json: true,
+										body: { ip_address: ip, unix_timestamp: unix_timestamp }
+									}, function (error, response, body) {
+										if(response && response.statusCode == 200){
+											status_code = true;
+										}
+										retry++;
+									});
+								}
+							}
+						});
+					}
+				}
+
+				return ("Successfully triggered for IP address: " + ip);
+			}
+		}
+
+	},
+	events: {
+
+	},
+	methods: {
+
+	},
+	created() {
+
+	},
+	async started() {
+
+	},
+	async stopped() {
+
+	}
+};
